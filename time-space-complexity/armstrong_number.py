72e419d8be9652f45f74125e1cebfbf0a842cbd7
DeveloperDolon <dolonr718@gmail.com>
2026-02-16 21:34:09 +0600

    armstrong numbers algoritm added

---
+++ time-space-complexity/armstrong_number.py
@@ -0,0 +1,22 @@
+
+# the exercise of armstrong numbers
+num = 153;
+
+def checkArmstrong(num):
+    temp = num;
+    nod = len(str(temp));
+    total = 0;
+
+    while temp > 0:
+        ld = temp % 10;
+        total = total + (ld ** nod);
+        temp = temp // 10;
+
+    if total == num:
+        print("The number is armstrong number: ", num);
+    else: 
+        print("Then number is not armstrong number: ", num);
+
+
+checkArmstrong(num);
+checkArmstrong(76);
