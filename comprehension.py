6b6389e6c096dd0f42217a6b2bc01aa4637d4671
DeveloperDolon <dolonr718@gmail.com>
2026-06-27 22:22:13 +0600

    update user list and add additional examples for set and dictionary comprehensions

--- comprehension.py
+++ comprehension.py
@@ -1,12 +1,38 @@
 
-users = [
+users = [ #list comprehension
     "Dolon",
     "Durjoy",
     "Soumik",
     "Sowrav",
-    "Rakhi"
+    "Rakhi",
+    "Dolon",
+    "Durjoy"
 ];
 
 usersWithD = [user for user in users if user.startswith('D')];
 
-print(usersWithD);
+# print(usersWithD);
+
+recipes = { #set comprehension
+    "Masala Chai": ["ginger", "cardamom", "clove"],
+    "Elaichi Chai": ["cardamom", "milk"],
+    "Spicy Chai": ["ginger", "black paper", "clove"]
+}
+
+
+unique_spicies = {spice for ingredients in recipes.values() for spice in ingredients}
+# print(unique_spicies)
+
+
+#dictionary comprehension
+tea_prices_tk = {
+    "Masala Chai" : 40,
+    "Green Tea" : 50,
+    "Lemon Tea" : 200
+}
+
+tea_price_in_usd = {tea:price / 100 for tea, price in tea_prices_tk.items()};
+# print(tea_price_in_usd);
+
+
+#generator comprehension
