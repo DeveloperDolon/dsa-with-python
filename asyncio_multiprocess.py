650d27e18671eeb8f30f77563c72a0e2591dba56
Dolon Roy <dolon@geekify.com.au>
2026-06-28 16:47:59 +0600

    add logging functionality and fetch_url async example in asyncio_multiprocess.py

--- asyncio_multiprocess.py
+++ asyncio_multiprocess.py
@@ -1,6 +1,8 @@
 
 import asyncio;
+import time;
 from concurrent.futures import ProcessPoolExecutor;
+import threading;
 
 def encrypt(data):
     return f"🔒{data[::-1]}";
@@ -13,5 +15,20 @@ async def main():
         print(f'Data encrypted: {result}');
 
 
-if __name__ == '__main__':
-    asyncio.run(main());
+# making a logger 
+def logging():
+    while True:
+        time.sleep(1);
+        print(f"System health check🧑‍⚕️");
+
+
+async def fetch_url():
+    time.sleep(3);
+    print('Data retrieved from url!');
+
+threading.Thread(target=logging, daemon=True).start();
+
+asyncio.run(fetch_url());
+
+# if __name__ == '__main__':
+#     asyncio.run(main());
