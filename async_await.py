7aa1dedb28c485560e27a554bb898553fa280db6
Dolon Roy <dolon@geekify.com.au>
2026-06-28 16:03:57 +0600

    add async/await example for fetching URLs concurrently

---
+++ async_await.py
@@ -0,0 +1,15 @@
+
+import asyncio;
+import aiohttp;
+
+async def fetch_url(session, url):
+    async with session.get(url) as response: 
+        print(f'Fetched url {url} with status {response}');
+
+async def main():
+    urls = ['https://jsonplaceholder.typicode.com/posts'] * 3;
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_url(session, url) for url in urls];
+        await asyncio.gather(*tasks);
+
+asyncio.run(main());
