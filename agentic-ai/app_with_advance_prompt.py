d37cc7121d1f18e0d2c1e581554ada56e1fe94a3
Dolon Roy <dolon@geekify.com.au>
2026-06-30 12:26:29 +0600

    refactor: clean up whitespace and improve code readability in app_with_advance_prompt.py

--- agentic-ai/app_with_advance_prompt.py
+++ agentic-ai/app_with_advance_prompt.py
@@ -4,7 +4,6 @@ import os;
 
 load_dotenv();
 
-
 client = OpenAI(
     api_key=os.getenv("GOOGLE_API_KEY"),
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
@@ -15,11 +14,12 @@ def chat_bot():
         context = input('Ask something to my ai🔈(exit): ');
         if(context == 'exit'):
             return;
-    
+
+        # ---------> The example of system prompt and user prompt
         response = client.chat.completions.create(
             model="gemini-3.5-flash",
             messages=[
-                {   "role": "system",
+                {   "role": "system", 
                     "content": "You are a assistant for solve mathematical problem, you will just help for solve mathematics, also replay with bangla language. If anyone ask you another question then replay him a very interesting answer to him to chang the topic and ask a mathematical related question or problem."
                 },
                 {
