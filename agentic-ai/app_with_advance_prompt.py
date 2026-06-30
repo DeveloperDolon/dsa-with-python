cb62bb7547f146e693c6e12705f8016f94e64b6c
Dolon Roy <dolon@geekify.com.au>
2026-06-30 12:22:24 +0600

    add app_with_advance_prompt.py for AI chatbot implementation using OpenAI's Gemini model

---
+++ agentic-ai/app_with_advance_prompt.py
@@ -0,0 +1,34 @@
+from openai import OpenAI;
+from dotenv import load_dotenv;
+import os;
+
+load_dotenv();
+
+
+client = OpenAI(
+    api_key=os.getenv("GOOGLE_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+);
+
+def chat_bot():
+    while True:
+        context = input('Ask something to my ai🔈(exit): ');
+        if(context == 'exit'):
+            return;
+    
+        response = client.chat.completions.create(
+            model="gemini-3.5-flash",
+            messages=[
+                {   "role": "system",
+                    "content": "You are a assistant for solve mathematical problem, you will just help for solve mathematics, also replay with bangla language. If anyone ask you another question then replay him a very interesting answer to him to chang the topic and ask a mathematical related question or problem."
+                },
+                {
+                    "role": "user",
+                    "content": context
+                }
+            ]
+        );
+
+        print(response.choices[0].message.content)
+
+chat_bot();
